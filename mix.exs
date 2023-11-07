92089f956b3abcb4d26f4b3366a1ce9529c70358
Sergey Storchay <r8@r8.com.ua>
2023-11-08 00:29:34 +0200

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -19,15 +19,15 @@ defmodule AshAuthentication.Firebase.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :ex_firebase_auth]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ash_authentication, "~> 3.11"},
+      {:ex_firebase_auth, "~> 0.5.1"},
     ]
   end
 
