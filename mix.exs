8f275fa507b9450c574e1383f0cc77d4bfd1df5b
Sergey Storchay <r8@r8.com.ua>
2024-10-29 12:55:19 +0200

    Add ex_doc to dependencies

--- mix.exs
+++ mix.exs
@@ -31,7 +31,8 @@ defmodule AshAuthentication.Firebase.MixProject do
       {:ash_authentication, "~> 3.11"},
       {:jose, ">= 1.10.0"},
       {:jason, ">= 1.4.0"},
-      {:finch, ">= 0.13.0"}
+      {:finch, ">= 0.13.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
